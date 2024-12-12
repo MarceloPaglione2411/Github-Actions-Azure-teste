372a7ce86cc6e7a7f063b21acbcb7bf9fed00a41
Marcelo Paglione <104773898+MarceloPaglione2411@users.noreply.github.com>
2024-12-12 15:35:35 -0300

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,1 +1,12 @@
+#Variables block
+variable "rs_name" {
+    description = "Nome Resource group"
+    type = string
+    default = "marcelofiles2"
+}
 
+variable "rs_location" {
+    description = "Localização Resource group"
+    type = string
+    default = "eastus"
+}
