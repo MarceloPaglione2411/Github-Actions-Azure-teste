a1282b8907bc2ee1dbb67339f4d6e952fbf6f6f1
Marcelo Paglione <104773898+MarceloPaglione2411@users.noreply.github.com>
2024-12-12 17:17:35 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,26 @@
-data "azurerm_resource_group" "RG1" {
-  name = "Terraformdemo"
-}
-output "id" {
-  value = data.azurerm_resource_group.RG1.id
-}
-data "azurerm_storage_account" "SR1" {
-  name                = "terraformcodemarcelo"
-  resource_group_name = "Terraformdemo"
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+  backend "azurerm" {
+      resource_group_name  = "tfstate"
+      storage_account_name = "<storage_account_name>"
+      container_name       = "tfstate"
+      key                  = "terraform.tfstate"
+  }
+
 }
 
-data "azurerm_storage_container" "container" {
-  name               = "tfstatefile"
-  storage_account_id = data.azurerm_storage_account.SR1.id
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "state-demo-secure" {
+  name     = "state-demo"
+  location = "eastus"
 }
 
 #Criação RG
