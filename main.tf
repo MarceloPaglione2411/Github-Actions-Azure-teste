05a9c6203283ebcac24f42ae61a67914cd86ed0e
Marcelo Paglione <104773898+MarceloPaglione2411@users.noreply.github.com>
2024-12-12 15:58:05 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,3 +3,22 @@ resource "azurerm_resource_group" "mod_resource_group1" {
   name     = var.rs_name
   location = var.rs_location
 }
+
+#Criação VNET + SUBNETS
+resource "azurerm_virtual_network" "MOD_VNET01" {
+  name                = "VNET01"
+  location            = var.rs_location
+  resource_group_name = var.rs_name
+  address_space       = ["10.0.0.0/16"]
+  tags = {
+    name = "Ambiente"
+    environment = "Storage Account"
+  }
+}
+
+resource "azurerm_subnet" "MOD_SUB-LNX" {
+  name                 = "SUB-LNX"
+  resource_group_name  = var.rs_name
+  virtual_network_name = azurerm_virtual_network.MOD_VNET01.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
