767306c5a34d007a989d11ee0a96777f0ab83a1b
Marcelo Paglione <104773898+MarceloPaglione2411@users.noreply.github.com>
2024-12-12 17:21:28 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,15 +25,15 @@ resource "azurerm_resource_group" "state-demo-secure" {
 
 #Criação RG
 resource "azurerm_resource_group" "mod_resource_group1" {
-  name     = var.rs_name
-  location = var.rs_location
+  name     = "marcelofile2"
+  location = "eastus"
 }
 
 #Criação VNET + SUBNETS
 resource "azurerm_virtual_network" "MOD_VNET01" {
   name                = "VNET01"
-  location            = var.rs_location
-  resource_group_name = var.rs_name
+  location            = "eastus"
+  resource_group_name = "marcelofile2"
   address_space       = ["10.0.0.0/16"]
   tags = {
     name = "Ambiente"
