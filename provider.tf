db9a9a81b92b1c01ffeca8fd4e71881c36e6f926
Marcelo Paglione <104773898+MarceloPaglione2411@users.noreply.github.com>
2024-12-13 17:26:43 -0300

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,3 @@
 provider "azurerm" {
-  subscription_id = "dae8fd16-05d7-4921-a766-fbe5243444e1"
-  client_id       = "995ef769-21f0-447d-991c-e3f40e27780c"
-  client_secret   = "txj8Q~S2MieevpzML9t1IpWtOdXMzcmANVW5maHX"
-  tenant_id       = "83cb0ae7-fc14-4d67-a7ef-b7c4943b8492"
-  features {}  
+  
 }
